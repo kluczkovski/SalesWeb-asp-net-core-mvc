abba677392c5ed6e0bba63ddc32802d194bf4d9f
Everton Kluczkovski <kluczkovski@hotmail.com>
2019-06-01 10:56:47 +0100

    added new models;

--- SalesWeb/Models/Department.cs
+++ SalesWeb/Models/Department.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SalesWeb.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+    
         public Department()
         {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSelle(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public void RemoveSelle(Seller seller)
+        {
+            Sellers.Remove(seller);
+        }
+
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sellers.Sum(s => s.TotalSales(inicial, final));
         }
     }
 }
--- SalesWeb/Models/SalesWebContext.cs
+++ SalesWeb/Models/SalesWebContext.cs
@@ -12,8 +12,9 @@ namespace SalesWeb.Models
         {
         }
 
-
         public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
 
     }
 }
